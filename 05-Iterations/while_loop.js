501cdc96cf6516de1b3e303e997c1141e3da7ddd
Vaibhav Kumar Singh <vaibhavsingh46614@gmail.com>
2026-08-20 23:47:51 +0530

    while

---
+++ 05-Iterations/while_loop.js
@@ -0,0 +1,26 @@
+//while loop:
+
+let index = 0;
+while(index <= 20){
+  console.log(`Value of index is ${index}`)
+  index = index + 2
+}
+
+
+// while loop in array
+let myArray = ['x','y','z']
+
+let arr = 0;
+while(arr < myArray.length){
+  console.log(`Value is ${myArray[arr]}`)
+  arr = arr+1
+}
+
+// do-while loop:
+
+//let score = 11
+let score = 1
+do{
+  console.log(`Score is ${score}`);
+  score++;
+}while(score <= 10)
